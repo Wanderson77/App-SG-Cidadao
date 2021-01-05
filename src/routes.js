af32ed9ffcd1d202ebb936473389da9da32d92c6
Wanderson Aldo <wanderson.aldo@gmail.com>
2021-01-05 13:43:42 -0200

    Added eslint.json

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
 import { createStackNavigator } from 'react-navigation';
 
 import SignIn from './pages/signIn';
-//import SignUp from './pages/signUp';
-//import Main from './pages/main';
+// import SignUp from './pages/signUp';
+// import Main from './pages/main';
 
 const Routes = createStackNavigator({
   SignIn,
-  //SignUp,
-  //Main,
+  // SignUp,
+  // Main,
 });
 
 export default Routes;
